514b18f9ea33f1c75c6d48179eb4be247ec7c914
Surachai Chenchirotphiphat <slumpmax@gmail.com>
2018-04-10 18:37:46 +0700

    add

---
+++ change.js
@@ -0,0 +1,31 @@
+var img2 = new Image;
+img2.src = "images/body_color.png";
+
+function conv(){
+
+var canvas = document.getElementById("canvas"),
+    image = document.getElementById("testImage"),
+    savedImageData = document.getElementById("imageData");
+
+ctx = canvas.getContext("2d");
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+ctx.drawImage(img2,0,0);
+ctx.drawImage(image,0,0);
+  
+var imgd = ctx.getImageData(0, 0, canvas.width, canvas.height),
+    pix = imgd.data,
+    uniqueColor = [255,200,0]; // Blue for an example, can change this value to be anything.
+
+// Loops through all of the pixels and modifies the components.
+for (var i = 0, n = pix.length; i <n; i += 4) {
+ //     if ((pix[i]==255) && (pix[i+1]==200) && (pix[i+2]==162)) {
+        imgd.data[i] = uniqueColor[0];   // Red component
+        imgd.data[i+1] = uniqueColor[1]; // Blue component
+        imgd.data[i+2] = uniqueColor[2]; // Green component
+ //     }
+}
+ctx.putImageData(imgd, 0, 0);
+
+savedImageData.src = canvas.toDataURL("image/png"); 
+}
--- dressup.js
+++ dressup.js
@@ -1,69 +1,69 @@
 $(document).ready(function(){
-
-function ImageSwitcher(choices, i) {
-	i = 0;
 	
-	this.Next = function() {
-		hide_current_image();
-		show_next_image();
-	}
-	this.Select = function(n) {
-		hide_current_image();
-		if(n>0) {
-			i = n - 1;
+	function ImageSwitcher(choices, i) {
+		i = 0;
+		
+		this.Next = function() {
+			hide_current_image();
 			show_next_image();
 		}
-	}
-	
-	var hide_current_image = function() {
-		if(choices){
-			choices[i].style.visibility = "hidden";
-			i += 1;
-		}
-	}
-	var show_next_image = function() {
-		if(choices){
-			if(i == (choices.length)) {
-				i = 0;
+		this.Select = function(n) {
+			hide_current_image();
+			if(n>0) {
+				i = n - 1;
+				show_next_image();
 			}
-			choices[i].style.visibility = "visible";
+		}
+	
+		var hide_current_image = function() {
+			if(choices){
+				choices[i].style.visibility = "hidden";
+				i += 1;
+			}
+		}
+		var show_next_image = function() {
+			if(choices){
+				if(i == (choices.length)) {
+					i = 0;
+				}
+				choices[i].style.visibility = "visible";
+			}
 		}
 	}
-}
   
-  var hairs = $(".hair");
-  var foreheads = $(".forehead");
-  var collars = $(".collar");
-  var sleeves = $(".sleeve");
-  var chairs = $(".chair");
-  var cforeheads = $(".cforehead");
-  var ccollars = $(".ccollar");
-  var csleeves = $(".csleeve");
+	var hairs = $(".hair");
+	var foreheads = $(".forehead");
+	var collars = $(".collar");
+	var sleeves = $(".sleeve");
+	var chairs = $(".chair");
+	var cforeheads = $(".cforehead");
+	var ccollars = $(".ccollar");
+	var csleeves = $(".csleeve");
 
-  var chair_picker = new ImageSwitcher(chairs);
-  var cforehead_picker = new ImageSwitcher(cforeheads);
-  var ccollar_picker = new ImageSwitcher(ccollars);
-  var csleeve_picker = new ImageSwitcher(csleeves);
+	var chair_picker = new ImageSwitcher(chairs);
+	var cforehead_picker = new ImageSwitcher(cforeheads);
+	var ccollar_picker = new ImageSwitcher(ccollars);
+	var csleeve_picker = new ImageSwitcher(csleeves);
 
-  var hair_picker = new ImageSwitcher(hairs);
-  document.getElementById("hair_button").onclick = function() { hair_picker.Next(); chair_picker.Next()};
+	var hair_picker = new ImageSwitcher(hairs);
+	document.getElementById("hair_button").onclick = function() { hair_picker.Next(); chair_picker.Next()};
 	
-  var forehead_picker = new ImageSwitcher(foreheads);
-  document.getElementById("forehead_button").onclick = function() {forehead_picker.Next(); cforehead_picker.Next(); };
+	var forehead_picker = new ImageSwitcher(foreheads);
+	document.getElementById("forehead_button").onclick = function() {forehead_picker.Next(); cforehead_picker.Next(); };
 
-  var collar_picker = new ImageSwitcher(collars);
-  document.getElementById("collar_button").onclick = function() {collar_picker.Next(); ccollar_picker.Next(); };
+	var collar_picker = new ImageSwitcher(collars);
+	document.getElementById("collar_button").onclick = function() {collar_picker.Next(); ccollar_picker.Next(); };
 
-  var sleeve_picker = new ImageSwitcher(sleeves);
-  document.getElementById("sleeve_button").onclick = function() {sleeve_picker.Next(); csleeve_picker.Next(); };
+	var sleeve_picker = new ImageSwitcher(sleeves);
+	document.getElementById("sleeve_button").onclick = function() {sleeve_picker.Next(); csleeve_picker.Next(); };
 
-hair_picker.Select(1);
-forehead_picker.Select(1);
-collar_picker.Select(1);
-sleeve_picker.Select(1);
+	hair_picker.Select(1);
+	forehead_picker.Select(1);
+	collar_picker.Select(1);
+	sleeve_picker.Select(1);
 
-chair_picker.Select(1);
-cforehead_picker.Select(1);
-ccollar_picker.Select(1);
-csleeve_picker.Select(1);
+	chair_picker.Select(1);
+	cforehead_picker.Select(1);
+	ccollar_picker.Select(1);
+	csleeve_picker.Select(1);
 });
