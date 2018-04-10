dcb71f6a2f4eab01ef106d4bec6276a4ff0567e4
Surachai Chenchirotphiphat <slumpmax@gmail.com>
2018-04-10 21:34:19 +0700

    test

--- change.js
+++ change.js
@@ -1,34 +1,32 @@
-var img2 = new Image;
-img2.src = "images/body_color.png";
+var img2 = new Image,
+  canvas2 = document.createElement('canvas'),
+  ctx2 = canvas2.getContext("2d"),
+  map2 = ctx2.getImageData(0,0,canvas2.width,canvas2.height),
+  pix2 = map2.data;
+
+img2.src = "images/body_color.png",
+canvas2.width = img2.width;
+canvas2.height = img2.height;
+ctx2.drawImage(img2,0,0);
+
+// Loops through all of the pixels and modifies the components.
+for (var i = 0, n = pix2.length; i <n; i += 4) {
+        map2.data[i] = uniqueColor[0];   // Red component
+        pix2[i+1] = uniqueColor[1]; // Blue component
+        pix2[i+2] = uniqueColor[2]; // Green component
+}
+ctx2.putImageData(map2, 0, 0);
 
 function conv(){
 
 var canvas = document.getElementById("canvas"),
     image = document.getElementById("testImage"),
-    savedImageData = document.getElementById("imageData");
-
-ctx = canvas.getContext("2d");
-
-ctx.clearRect(0,0,canvas.width,canvas.height);
-var map2 = img2.getImageData(0,0,img2.width,img2.height);
-var pix2 = map2.data;
-//ctx.drawImage(img2,0,0);
-
-  
-var imgd = ctx.getImageData(0, 0, canvas.width, canvas.height),
-    pix = imgd.data,
+    savedImageData = document.getElementById("imageData"),
+    ctx = canvas.getContext("2d"),
     uniqueColor = [255,200,192]; // Blue for an example, can change this value to be anything.
 
-// Loops through all of the pixels and modifies the components.
-for (var i = 0, n = pix.length; i <n; i += 4) {
-
-        imgd.data[i] = uniqueColor[0];   // Red component
-        imgd.data[i+1] = uniqueColor[1]; // Blue component
-        pix[i+2] = uniqueColor[2]; // Green component
-        pix[i+3] = pix2[i+3];
-    
-}
-ctx.putImageData(imgd, 0, 0);
+ctx.clearRect(0,0,canvas.width,canvas.height);
+ctx.drawImage(img2,0,0);
 ctx.drawImage(image,0,0);
 
 savedImageData.src = canvas.toDataURL("image/png"); 
