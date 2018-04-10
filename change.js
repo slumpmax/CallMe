bb1045c6a0f75c61882cf8ff5a86bb6880e7519a
Surachai Chenchirotphiphat <slumpmax@gmail.com>
2018-04-10 19:57:55 +0700

    Update change.js

--- change.js
+++ change.js
@@ -10,22 +10,25 @@ var canvas = document.getElementById("canvas"),
 ctx = canvas.getContext("2d");
 
 ctx.clearRect(0,0,canvas.width,canvas.height);
-ctx.drawImage(img2,0,0);
-ctx.drawImage(image,0,0);
+var map2 = img2.getImageData(0,0,img2.width,img2.height);
+var pix2 = map2.data;
+//ctx.drawImage(img2,0,0);
+
   
 var imgd = ctx.getImageData(0, 0, canvas.width, canvas.height),
     pix = imgd.data,
-    uniqueColor = [255,200,0]; // Blue for an example, can change this value to be anything.
+    uniqueColor = [255,200,192]; // Blue for an example, can change this value to be anything.
 
 // Loops through all of the pixels and modifies the components.
 for (var i = 0, n = pix.length; i <n; i += 4) {
-    if ((pix[i]==255) && (pix[i+1]==200) && (pix[i+2]==162)) {
+    if (pix2[i+3]!=0){
         imgd.data[i] = uniqueColor[0];   // Red component
         imgd.data[i+1] = uniqueColor[1]; // Blue component
-        imgd.data[i+2] = uniqueColor[2]; // Green component
+        pix[i+2] = uniqueColor[2]; // Green component
     }
 }
 ctx.putImageData(imgd, 0, 0);
+ctx.drawImage(image,0,0);
 
 savedImageData.src = canvas.toDataURL("image/png"); 
 }
