6fb64ea6c8ee020007e615a09d46e9ab1e7e5d25
Surachai Chenchirotphiphat <slumpmax@gmail.com>
2018-04-10 19:32:53 +0700

    Update change.js

--- change.js
+++ change.js
@@ -19,7 +19,7 @@ var imgd = ctx.getImageData(0, 0, canvas.width, canvas.height),
 
 // Loops through all of the pixels and modifies the components.
 for (var i = 0, n = pix.length; i <n; i += 4) {
- //     if ((pix[i]==255) && (pix[i+1]==200) && (pix[i+2]==162)) {
+    if ((pix[i]==255) && (pix[i+1]==200) && (pix[i+2]==162)) {
         imgd.data[i] = uniqueColor[0];   // Red component
         imgd.data[i+1] = uniqueColor[1]; // Blue component
         imgd.data[i+2] = uniqueColor[2]; // Green component
